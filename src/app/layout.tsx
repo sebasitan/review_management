3ee6634d0a56457542f8d190be4132d1b0ec8213
stallioninet <78951510+stallioninet@users.noreply.github.com>
2026-01-07 14:48:13 +0530

    Design: Add custom SVG favicon and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body suppressHydrationWarning>
         <NextAuthProvider>
